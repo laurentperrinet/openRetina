8f4416d1929509e2ed41a92eadd2dd6dd94d9a3f
Laurent Perrinet <laurent.perrinet@gmail.com>
2015-08-27 16:54:44 +0200

    stream RGB to numpy and capture

--- client.py
+++ client.py
@@ -15,6 +15,7 @@ connection = client_socket.makefile('wb')
 try:
     with picamera.PiCamera() as camera:
         camera.resolution = (w, h)
+        camera.framerate = 30
         # Start a preview and let the camera warm up for 2 seconds
         camera.start_preview()
         time.sleep(2)
