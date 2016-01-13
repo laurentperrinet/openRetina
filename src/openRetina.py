b4745e0e1b44bdccc3e38560d2133b3aa0e18e79
David Arbib <davidarbib@yahoo.fr>
2016-01-13 17:07:47 +0100

    trycatch import vispy and Canvas

--- src/openRetina.py
+++ src/openRetina.py
@@ -43,7 +43,6 @@ class PhotoReceptor:
 
             try:
                 self.cap = cv2.VideoCapture(0)
-                print (self.cap != NULL)
                 if not self.cap.isOpened(): toto
 
                 self.DOWNSCALE = DOWNSCALE
@@ -308,74 +307,76 @@ class openRetina(object):
         A = np.frombuffer(msg, dtype=md['dtype'])
         return A.reshape(md['shape'])
 
-from vispy import app
-from vispy import gloo
+    try : 
+        from vispy import app
+        from vispy import gloo
 
-vertex = """
-    attribute vec2 position;
-    attribute vec2 texcoord;
-    varying vec2 v_texcoord;
-    void main()
-    {
-        gl_Position = vec4(position, 0.0, 1.0);
-        v_texcoord = texcoord;
-    }
-"""
+        vertex = """
+            attribute vec2 position;
+            attribute vec2 texcoord;
+            varying vec2 v_texcoord;
+            void main()
+            {
+                gl_Position = vec4(position, 0.0, 1.0);
+                v_texcoord = texcoord;
+            }
+        """
 
-fragment = """
-    uniform sampler2D texture;
-    varying vec2 v_texcoord;
-    void main()
-    {
-        gl_FragColor = texture2D(texture, v_texcoord);
+        fragment = """
+            uniform sampler2D texture;
+            varying vec2 v_texcoord;
+            void main()
+            {
+                gl_FragColor = texture2D(texture, v_texcoord);
 
-        // HACK: the image is in BGR instead of RGB.
-        float temp = gl_FragColor.r;
-        gl_FragColor.r = gl_FragColor.b;
-        gl_FragColor.b = temp;
-    }
-"""
+                // HACK: the image is in BGR instead of RGB.
+                float temp = gl_FragColor.r;
+                gl_FragColor.r = gl_FragColor.b;
+                gl_FragColor.b = temp;
+            }
+        """
 
-class Canvas(app.Canvas):
-    def __init__(self, retina):
-        app.use_app('pyglet')
-        self.retina = retina
-        app.Canvas.__init__(self, keys='interactive', fullscreen=True, size=(1280, 960))#
-        self.program = gloo.Program(vertex, fragment, count=4)
-        self.program['position'] = [(-1, -1), (-1, +1), (+1, -1), (+1, +1)]
-        self.program['texcoord'] = [(1, 1), (1, 0), (0, 1), (0, 0)]
-        self.program['texture'] = np.zeros((self.retina.h, self.retina.w, 3)).astype(np.uint8)
-        width, height = self.physical_size
-        gloo.set_viewport(0, 0, width, height)
-        self._timer = app.Timer('auto', connect=self.on_timer, start=True)
-        self.start = time.time()
-        self.show()
+        class Canvas(app.Canvas):
+            def __init__(self, retina):
+                app.use_app('pyglet')
+                self.retina = retina
+                app.Canvas.__init__(self, keys='interactive', fullscreen=True, size=(1280, 960))#
+                self.program = gloo.Program(vertex, fragment, count=4)
+                self.program['position'] = [(-1, -1), (-1, +1), (+1, -1), (+1, +1)]
+                self.program['texcoord'] = [(1, 1), (1, 0), (0, 1), (0, 0)]
+                self.program['texture'] = np.zeros((self.retina.h, self.retina.w, 3)).astype(np.uint8)
+                width, height = self.physical_size
+                gloo.set_viewport(0, 0, width, height)
+                self._timer = app.Timer('auto', connect=self.on_timer, start=True)
+                self.start = time.time()
+                self.show()
 
-    def on_resize(self, event):
-        width, height = event.physical_size
-        gloo.set_viewport(0, 0, width, height)
+            def on_resize(self, event):
+                width, height = event.physical_size
+                gloo.set_viewport(0, 0, width, height)
 
-    def on_draw(self, event):
-        gloo.clear('black')
-        if self.retina.verb: print("Sending request")
-        if time.time()-self.start < self.retina.model['T_SIM']: # + ret.sleep_time*2: 
-            self.retina.socket.send (b"Hello")
-        else:
-            sys.exit()
-        data = self.retina.recv_array(self.retina.socket)
-        if self.retina.verb: 
-            print("Received reply ", data.shape, data.min(), data.max())
-        image = self.retina.decode(data)
-        self.program['texture'][...] = (image*128).astype(np.uint8)
-        self.program.draw('triangle_strip')
-
-
-    def on_timer(self, event):
-        self.update()
+            def on_draw(self, event):
+                gloo.clear('black')
+                if self.retina.verb: print("Sending request")
+                if time.time()-self.start < self.retina.model['T_SIM']: # + ret.sleep_time*2: 
+                    self.retina.socket.send (b"Hello")
+                else:
+                    sys.exit()
+                data = self.retina.recv_array(self.retina.socket)
+                if self.retina.verb: 
+                    print("Received reply ", data.shape, data.min(), data.max())
+                image = self.retina.decode(data)
+                self.program['texture'][...] = (image*128).astype(np.uint8)
+                self.program.draw('triangle_strip')
 
 
-if __name__ == '__main__':
-    c = Canvas()
-    app.run()
-    c.cap.release()
+            def on_timer(self, event):
+                self.update()
 
+
+        if __name__ == '__main__':
+            c = Canvas()
+            app.run()
+            c.cap.release()
+    except :
+        pass
