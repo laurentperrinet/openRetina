04de10ef1851b44530ded7de18fbaa532f9de8d1
Laurent Perrinet <laurent.perrinet@gmail.com>
2016-12-16 10:14:23 +0100

    makes it work again on MacOS

--- dev/thalamus.py
+++ dev/thalamus.py
@@ -1,17 +1,26 @@
+# -*- coding: utf8 -*-
+from __future__ import division, print_function
+"""
+openRetina : a photoreceptor layer
+
+See https://github.com/laurentperrinet/openRetina
+
+"""
+__author__ = "(c) Victor Boutin & Laurent Perrinet INT - CNRS"
 
 import subprocess
 p = subprocess.Popen(['/usr/local/bin/ipython3', 'photoreceptors.py'])
 from openRetina import openRetina
-thalamus = openRetina(model=dict(layer='thalamus', 
-    input=['stream'], output=['display', 'capture'], 
+thalamus = openRetina(model=dict(layer='thalamus',
+    input=['stream'], output=['display', 'capture'],
     T_SIM=120))
 thalamus.run()
 
 
 # import sys
 # sys.exit()
-# 
-# 
+#
+#
 # from concurrent.futures import ThreadPoolExecutor
 # from openRetina import openRetina
 # thalamus = openRetina(model=dict(layer='thalamus', input=['stream'], output=['display', 'capture']))
@@ -19,23 +28,23 @@ thalamus.run()
 # with ThreadPoolExecutor() as e:
 #     e.submit(thalamus.run)
 #     e.submit(phrs.run)
-# 
-# 
+#
+#
 # import sys
 # sys.exit()
-# 
-# 
+#
+#
 # from openRetina import openRetina
 # thalamus = openRetina(model=dict(layer='thalamus', input=['stream'], output=['display', 'capture']))
 # thalamus.run()
-# 
-# 
-# 
+#
+#
+#
 # from multiprocessing.pool import ThreadPool
 # pool = ThreadPool()
-# 
+#
 # phrs = openRetina(model=dict(layer='phrs', input=['opencv'], output=['stream']))
-# 
+#
 # import multiprocessing
 # p2 = multiprocessing.Process(target=thalamus.run, args=())
 # print(p2, p2.is_alive())
@@ -45,7 +54,7 @@ thalamus.run()
 # print(p1, p1.is_alive())
 # p1.start()
 # print(p1, p1.is_alive())
-# 
+#
 # import sys
 # sys.exit()
 # import threading
@@ -57,14 +66,14 @@ thalamus.run()
 # print(p1, p1.is_alive())
 # p1.start()
 # print(p1, p1.is_alive())
-# 
+#
 # threads = [p1, p2]
 # for thread in threads:
 #     thread.join()
-# 
+#
 # import sys
 # sys.exit()
-# 
+#
 # from multiprocessing.popen_fork import Pool
 # from openRetina import openRetina
 # # thalamus = openRetina(model=dict(layer='thalamus', input=['stream'], output=['display', 'capture']))
@@ -75,21 +84,21 @@ thalamus.run()
 #         self.phrs = openRetina(model=dict(layer='phrs', input=['opencv'], output=['stream']))
 #         self.event = threading.Event()
 #         self.start()
-# 
+#
 #     def run(self):
 #         # This method runs in a background thread
 #         self.phrs.run()
-# 
+#
 # class ThalamusStreamer(threading.Thread):
 #     def __init__(self):
 #         super(ThalamusStreamer, self).__init__()
 #         self.thalamus = openRetina(model=dict(layer='thalamus', input=['stream'], output=['display', 'capture']))
 #         self.event = threading.Event()
 #         self.start()
-# 
+#
 #     def run(self):
 #         # This method runs in a background thread
 #         self.thalamus.run()
-# 
+#
 # pool_lock = threading.Lock()
 # pool =[RetinaStreamer(), ThalamusStreamer()]
