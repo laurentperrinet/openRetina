ac298e9e91ceefa0d7a99408547aa8251faeb025
Laurent Perrinet <laurent.perrinet@gmail.com>
2016-12-19 17:14:19 +0100

    managed to launch two processes in parallel

--- dev/noise_display.py
+++ dev/noise_display.py
@@ -10,22 +10,74 @@ __author__ = "(c) Victor Boutin & Laurent Perrinet INT - CNRS"
 
 from openRetina import openRetina
 
-import multiprocessing
-phrs = openRetina(model=dict(layer='test_noise', # label for this layer
-                             input=['noise'], # input: can be the camera, noise, a movie (TODO)
-                             output=['stream'] # output: can be stream, display, ...
-                             ))
 
-thread_noise = multiprocessing.Process(target=phrs.run)
-thread_noise.start()
+import threading
+import time
 
+exitFlag = 0
 
+class myThread (threading.Thread):
+    def __init__(self, threadID, name, input, output):
+        threading.Thread.__init__(self)
+        self.threadID = threadID
+        self.name = name
+        self.node = openRetina(model=dict(layer=name, # label for this layer
+                                     input=input, # input: can be the camera, noise, a movie (TODO)
+                                     output=output, # output: can be stream, display, ...
+                                     T_SIM=120))
+    def run(self):
+        print ("Starting " + self.name)
+        self.node.run()
+        print ("Exiting " + self.name)
+
+def print_time(threadName, delay, counter):
+    while counter:
+        if exitFlag:
+            threadName.exit()
+        time.sleep(delay)
+        print ("%s: %s" % (threadName, time.ctime(time.time())))
+        counter -= 1
+
+# Create new threads
+thread1 = myThread(1, 'test_noise', input=['noise'], output=['stream'])
+# thread2 = myThread(2, 'test_display', # label for this layer
+#                              input=['stream'], # input: can be the camera, noise, a movie (TODO)
+#                              output=['display'], # output: can be stream, display, capture,...
+#                              )
+
+# Start new Threads
+thread1.start()
+#thread2.start()
 
 thalamus = openRetina(model=dict(layer='test_display', # label for this layer
                              input=['stream'], # input: can be the camera, noise, a movie (TODO)
                              output=['display'], # output: can be stream, display, capture,...
                              T_SIM=120))
 
-#thalamus.run()
-thread_display = multiprocessing.Process(target=thalamus)
-thread_display.start()
+thalamus.run()
+
+thread1.join()
+#thread2.join()
+
+print ("Exiting Main Thread")
+
+#
+# import multiprocessing
+# phrs = openRetina(model=dict(layer='test_noise', # label for this layer
+#                              input=['noise'], # input: can be the camera, noise, a movie (TODO)
+#                              output=['stream'] # output: can be stream, display, ...
+#                              ))
+#
+# thread_noise = multiprocessing.Process(target=phrs.run)
+# thread_noise.start()
+#
+#
+#
+# thalamus = openRetina(model=dict(layer='test_display', # label for this layer
+#                              input=['stream'], # input: can be the camera, noise, a movie (TODO)
+#                              output=['display'], # output: can be stream, display, capture,...
+#                              T_SIM=120))
+#
+# #thalamus.run()
+# thread_display = multiprocessing.Process(target=thalamus)
+# thread_display.start()
