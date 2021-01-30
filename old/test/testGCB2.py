4f3cb802bab296a8ef058a58f775a2ef5d93dcb5
Laurent Perrinet <laurent.perrinet@univ-amu.fr>
2021-01-30 17:14:20 +0100

    removing old stuff

--- old/test/testGCB2.py
+++
@@ -1,7 +0,0 @@
-import GCB2 as comm
-
-a=comm.GCB_REP_REQ(
-    rep_port=['6665'],
-    verbose=True)
-
-a.REP_array()
--- old/test/testGCB2b.py
+++
@@ -1,8 +0,0 @@
-import GCB2 as comm
-
-b=comm.GCB_REP_REQ(
-    req_ip=['localhost'],
-    req_port=['6665'],
-    #rep_port=['6666'],
-    verbose=True)
-b.REQ_array()
