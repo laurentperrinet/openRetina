868902d1e094b37a82d22b438aa2accaaf7b959d
Laurent Perrinet <laurent.perrinet@univ-amu.fr>
2021-01-31 21:28:16 +0100

    Update thalamus.py

--- dev/thalamus.py
+++ dev/thalamus.py
@@ -12,6 +12,7 @@ __author__ = "(c) Pierre Albiges, Victor Boutin & Laurent Perrinet INT - CNRS"
 from eventRetina import eventRetina
 thalamus = eventRetina(model=dict(layer='thalamus', # label for this layer
                              input=['stream'], # input: can be the camera, noise, a movie (TODO)
+                             ip='192.168.0.18',
                              output=['display'],
                              # output=['display','capture'],
                              #output=['capture'], # output: can be stream, display, capture,...
