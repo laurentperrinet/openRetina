58686b252826c16a284fee3ad20c3562d12305a2
Laurent Perrinet <laurent.perrinet@univ-amu.fr>
2022-05-16 10:59:08 +0200

    Update thalamus.py

--- dev/thalamus.py
+++ dev/thalamus.py
@@ -10,10 +10,10 @@ __author__ = "(c) Pierre Albiges, Victor Boutin & Laurent Perrinet INT - CNRS"
 # p = subprocess.Popen(['python3 photoreceptors.py'])
 
 from eventRetina import eventRetina
-thalamus = eventRetina(model=dict(layer='thalamus', # label for this layer
-                             input=['stream'], # input: can be the camera, noise, a movie (TODO)
-                             # output=['display'],
-                             output=['display','capture'],
-                             #output=['capture'], # output: can be stream, display, capture,...
-                             T_SIM=20))
+thalamus = eventRetina(model=dict(layer='thalamus',  # label for this layer
+                                  input=['stream'],  # input: can be the camera, noise, a movie (TODO)
+                                  # output=['display'],
+                                  output=['display', 'capture'],
+                                  #output=['capture'], # output: can be stream, display, capture,...
+                                  T_SIM=20))
 thalamus.run()
